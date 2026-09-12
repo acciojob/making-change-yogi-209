717ca73dd11e9877b43760a92e570a605efe817d
Boyanapalli Yogendra sai <boyanapalliyogendrasai@gmail.com>
2026-09-12 11:20:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const makeChange = (c) => {
+	c=parseInt(c,10);
+	const q=Math.floor(c/25);
+	c%=25;
+
+	const d=Math.floor(c/10);
+	c%=10;
+
+	const n=Math.floor(c/5);
+	c%=5;
+
+	const p=c;
+
+	return q,d,n,p;
   // your name here
 };
 
